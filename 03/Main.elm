4af0877f32ddd65f448714b22d3153d8187b30f5
Francisco Sales <franciscofsales1@gmail.com>
2016-11-15 22:50:44 +0000

    ex03

---
+++ 03/Main.elm
@@ -0,0 +1,38 @@
+module Main exposing (..)
+
+import Html exposing (text)
+import List
+
+type alias Person = {
+    name : String
+    , age : Int
+}
+
+people =
+  [
+  { name = "Legolas", age = 2931},
+  { name = "Gimli", age = 130}
+  ]
+
+names : List Person -> List String
+names peeps =
+  List.map (\peep -> peep.name) peeps
+
+findPerson : String -> List Person -> Maybe Person
+findPerson name peeps =
+  List.foldl
+    (\peep memo ->
+      case memo of
+        Just _ ->
+          memo
+        Nothing ->
+          if peep.name == name then
+            Just peep
+          else
+            Nothing
+    )
+    Nothing
+    peeps
+
+main =
+  text <| toString <| findPerson "Legolas" people
