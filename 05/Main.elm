a6dfda30d3dd295f583ca4625c4628dd922b1860
Francisco Sales <franciscofsales1@gmail.com>
2016-11-15 23:41:51 +0000

    ex05,06

---
+++ 05/Main.elm
@@ -0,0 +1,19 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import List
+
+numbers =
+  [ 1, 2, 3, 4, 5 ]
+
+printThing : thing -> Html msg
+printThing thing =
+  ul [] [ text <| toString thing ]
+
+fruits =
+  [ { name = "Orange" }, { name = "Banana"} ]
+
+
+main =
+  ul [] (List.map printThing numbers)
