5bbf79ac4e87e7845d00c92e5e8db9f3c0b83fa6
Francisco Sales <franciscofsales1@gmail.com>
2016-11-15 23:57:12 +0000

    07

---
+++ 07/Main.elm
@@ -0,0 +1,11 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import String.Extra exposing (pluralize)
+items =
+  [ "Green Eggs", "Green Ham" ]
+
+main =
+  div []
+      [ h1 [] [ text <| (pluralize "Item" "Items" (List.length items)) ]
+      ]
